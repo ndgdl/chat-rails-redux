e42c74eb81b06c2bd7da95bba306c477bf86bfff
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-04-06 18:55:01 +0200

    improved with as_json overwritten in model

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,31 +2,18 @@ class Api::V1::MessagesController < ApplicationController
   before_action :set_channel
 
   def index
-    messages = @channel.messages.map do |message|
-      {
-        id: message.id,
-        author: message.user.email,
-        content: message.content,
-        created_at: message.created_at
-      }
-    end
+    messages = @channel.messages
+    # .order(:created_at, :asc)
 
     render json: messages
   end
 
   def create
-    message = current_user.messages.build(content: params[:content])
+    message = current_user.messages.build(message_params)
     message.channel = @channel
     message.save
 
-    json_message = {
-      id: message.id,
-      author: message.user.email,
-      content: message.content,
-      created_at: message.created_at
-    }
-
-    render json: json_message
+    render json: message
   end
 
   private
@@ -35,7 +22,7 @@ class Api::V1::MessagesController < ApplicationController
     @channel = Channel.find_by(name: params[:channel_id])
   end
 
-  # def message_params
-  #   params.require(:message).permit(:content)
-  # end
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,13 @@ class Message < ApplicationRecord
   belongs_to :channel
 
   validates :content, presence: true
+
+  def as_json(options = {})
+    {
+      id: id,
+      author: user.email,
+      content: content,
+      created_at: created_at
+    }
+  end
 end
